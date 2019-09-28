4b809eaca873df1f1e4f3a42c919f3e57ccb8776
SandunRathsara <SandunRathsara@users.noreply.github.com>
2019-09-29 00:52:50 +0530

    no message

--- IT18028706.py
+++ IT18028706.py
@@ -1,36 +1,37 @@
 def main():
 
-    # VALIDATING THE INPUT OF 'm' and 'n'
+    # THIS LOOP CONTINUES THE PROGRAM UNTIL THE USER INPUTS '0 0'
     while True:
-        m = int(input("Enter the number of regions:"))
-        if m < 0 or m > 10:
-            print("number of regions cannot be more than 10 or lower than 1!")
-            continue
-        n = int(input("Enter the number of programs: "))
-        if n < 0 or n > 50:
-            print("number of programs cannot be more than 50 or less than 1!")
-            continue
-        if n == 0 and m == 0:
-            print("Thank you!")
-            exit()
-        break
+        # VALIDATING THE INPUT OF 'm' and 'n'
+        while True:
+            s, r = input("Enter the number of regions and number of programs(0 0 to exit):").split()
+            m = int(s)
+            n = int(r)
+            if m < 0 or m > 10:
+                print("number of regions cannot be more than 10 or lower than 1!")
+                continue
+            if n < 0 or n > 50:
+                print("number of programs cannot be more than 50 or less than 1!")
+                continue
+            if n == 0 and m == 0:
+                print("Thank you!")
+                exit()
+            break
 
-    # TAKING MEMORY REGIONS SIZE
-    memory_size = []
-    for i in range(m):
-        size = int(input("Enter the size of the region "+str(i+1)+":"))
-        memory_size.append(size)
+        # TAKING MEMORY REGIONS SIZE
+        memory_size = []
+        for i in range(m):
+            size = int(input("Enter the size of the region "+str(i+1)+":"))
+            memory_size.append(size)
 
-    # FIRST TEST CASE PROGRAM LIST
-    prog_list1 = input_prog(n)
-
-    print(prog_list1)
+        # TESTING
+        print(input_prog(n))
 
 
 # TAKING INPUT FOR PROGRAM LIST
 def input_prog(n):
     # CREATING A LIST TO STORE PROGRAM LIST
-    prog_list = [[0 for x in range(1)] for y in range(n)]
+    prog_list = [[0] for y in range(n)]
 
     # TAKING INPUT TO PROGRAM LIST
     for i in range(n):
@@ -55,5 +56,6 @@ def input_prog(n):
 
     # RETURNING THE PROGRAM LIST
     return prog_list
+
 
 main()
