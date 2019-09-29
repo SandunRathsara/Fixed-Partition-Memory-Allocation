25a576b20889d38e0fe680521e63abd0eaef101c
SandunRathsara <SandunRathsara@users.noreply.github.com>
2019-09-29 22:48:56 +0530

    added get_lowest_exec_time function

--- Allocate.py
+++ Allocate.py
@@ -1,5 +1,6 @@
 def main():
 
+    case = 1
     # THIS LOOP CONTINUES THE PROGRAM UNTIL THE USER INPUTS '0 0'
     while True:
         # VALIDATING THE INPUT OF 'm' and 'n'
@@ -25,7 +26,9 @@ def main():
             memory_size.append(size)
 
         # TESTING
-        print(input_prog(n))
+        prog_list = input_prog(n)
+        print(prog_list)
+        print(allocate(n, m, prog_list, memory_size))
 
 
 # TAKING INPUT FOR PROGRAM LIST
@@ -56,6 +59,18 @@ def input_prog(n):
 
     # RETURNING THE PROGRAM LIST
     return prog_list
+
+
+# FUNCTION TO SELECT THE MINIMUM SPACE INDEX IF THE K VALUE IS GREATER THAN 1
+def get_lowest_exec_time(program=[]):
+    min_time = 9223372036854775807
+    min_size_index = -1
+    for i in range(1, len(program)):
+        if i % 2 == 0:
+            if min_time > program[i]:
+                min_time = program[i]
+                min_size_index = i - 1
+    return min_size_index
 
 
 main()
